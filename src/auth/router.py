412f9574baa2c8290e0d4837198371839c416a98
Vlad Frolov <frolov360180@gmail.com>
2023-02-12 14:26:02 +0300

    lesson 12.02.2023

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,4 +1,5 @@
-from typing import List
+import datetime
+from typing import List, Callable
 
 from fastapi import APIRouter, Depends, HTTPException, status, Form
 from sqlalchemy import select, insert, update, delete
@@ -14,66 +15,13 @@ from .schemas import User, UserPatch, UserLogin
 
 from jose import jwt
 
-
 auth_router = APIRouter(
     prefix='/auth',
     tags=['Auth']
 )
 
-
-@auth_router.get('/users')
-async def get_users(session: AsyncSession = Depends(get_session)) -> List[User]:
-    query = select(users)
-    result = await session.execute(query)
-    return result.all()
-
-
-@auth_router.get('/user/{id}')
-async def get_user_by_id(id: int, session: AsyncSession = Depends(get_session)) -> User:
-    user = select(users).where(users.c.id == id)
-    result = await session.execute(user)
-    return result.first()
-
-
-@auth_router.post('/add_user/')
-async def get_users(user_data: User, session: AsyncSession = Depends(get_session)):
-    new_user = insert(users).values(**user_data.dict())
-    await session.execute(new_user)
-    await session.commit()
-    return {'status': 'success'}
-
-
-@auth_router.patch('/user_path/{id}')
-async def user_path(id: int, user_patch: UserPatch, session: AsyncSession = Depends(get_session)) -> User:
-    user = dict(user_patch)
-    for key in user.copy():
-        if user[key] == None:
-            user.pop(key)
-    update_user = update(users).where(users.c.id==id).values(**user)
-    await session.execute(update_user)
-    await session.commit()
-    updated_user = select(users).where(users.c.id==id)
-    result = await session.execute(updated_user)
-    return result.first()
-
-
-@auth_router.delete('/user/{id}')
-async def user_delete(id: int, session: AsyncSession = Depends(get_session)):
-    delete_user = delete(users).where(users.c.id==id)
-    await session.execute(delete_user)
-    await session.commit()
-    return {'result': 'success'}
-
-
-@auth_router.delete('/del/{id}')
-async def delete_user(id: int, session: AsyncSession = Depends(get_session)):
-    user = delete(users).where(users.c.id == id)
-    await session.execute(user)
-    return {'result': 'success'}
-
-
-
 secret = 'johan'
+
 
 @auth_router.post('/login')
 async def user_login(data: UserLogin, session: AsyncSession = Depends(get_session)):
@@ -81,13 +29,34 @@ async def user_login(data: UserLogin, session: AsyncSession = Depends(get_sessio
         user = select(users).where(users.c.email == data.email)
         get_user = await session.execute(user)
         if data.password == get_user.fetchone()[3]:
-            token = jwt.encode({'email': data.email, 'password': data.password}, secret, algorithm='HS256')
-            return {'bearer': token}
+            access = jwt.encode({'email': data.email, 'password': data.password,
+                                 "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=2)}, secret,
+                                algorithm='HS256')
+            refresh = jwt.encode({'email': data.email, 'password': data.password,
+                                 "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=5)}, secret,
+                                algorithm='HS256')
+            return {
+                'access': access,
+                'refresh': refresh
+            }
         else:
             return {"message": "Email or password not valid"}
 
     except Exception:
         return {"message": "Email or password not valid"}
+
+
+# def authorization(handler: Callable):
+#
+#     def inner(*args, **kwagrs):
+
+
+
+@auth_router.post('/authorization')
+async def authorization(request: Request, session: AsyncSession = Depends(get_session)):
+    access = request
+
+
 
 
 @auth_router.post('/auth')
@@ -105,5 +74,3 @@ async def get_authorization(request: Request, session: AsyncSession = Depends(ge
             detail="Invalid authentication credentials",
             headers={"WWW-Authenticate": "Bearer"},
         )
-
-
